a50a3ccde4624a802a99a694d516b4955caa3ec2
ChrisYuneva <christina_yuneva@mail.ru>
2021-03-14 11:19:27 +0400

    feat: add brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (typeof str !== 'string') return false;
+  let bracket = "";
+  let liken = "";
+  let count;
+
+  while (count !== bracketsConfig.length) {
+      count = 0;
+      for (i = 0; i < bracketsConfig.length; i++) {
+      bracket = bracketsConfig[i][0] + bracketsConfig[i][1];
+      liken = str.replace(bracket, "");
+      if (str === liken) {
+        count++;
+      }
+      str = liken;
+    }
+  }
+
+  if (str.length === 0) {
+      return true; 
+  } else {
+      return false;
+  }
 }
